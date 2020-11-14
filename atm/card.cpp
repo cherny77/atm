3c79e79f871cd9e7ccccb131a06b3379e98a85be
Volodymyr <volodymyr.y.rybak@gmail.com>
2020-11-14 15:57:50 +0200

    card.cpp changes

--- atm/card.cpp
+++ atm/card.cpp
@@ -1,2 +1,44 @@
 #include "card.h"
+#include "databaseapi.h"
+
 Card::Card(QString cardNumber, bool isBlocked): _cardNumber(new QString(cardNumber)), _isBlocked(isBlocked) {}
+
+void Card::withdraw(long long amount)
+{
+    if (!DataBaseApi::getDataBaseApi()->withdrawCard(*this->_cardNumber, amount))
+    {
+        throw "operation failed";
+    }
+}
+
+void Card::recharge(long long amount)
+{
+    if (!DataBaseApi::getDataBaseApi()->rechargeCard(*this->_cardNumber, amount))
+    {
+        throw "operation failed";
+    }
+}
+
+void Card::changePin(QString newPin)
+{
+    if (!DataBaseApi::getDataBaseApi()->changePinCard(*this->_cardNumber, newPin))
+    {
+        throw "operation failed";
+    }
+}
+
+void Card::transferTo(QString cardNumberTo, long long amount)
+{
+    if (!DataBaseApi::getDataBaseApi()->transaction(*this->_cardNumber, cardNumberTo, amount))
+    {
+        throw "operation failed";
+    }
+}
+
+void Card::block()
+{
+    if (!DataBaseApi::getDataBaseApi()->blockCard(*this->_cardNumber))
+    {
+        throw "operation failed";
+    }
+}
